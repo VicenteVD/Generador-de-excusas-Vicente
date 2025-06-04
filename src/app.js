e08b5d899241244230c2e34d4bccb350d6597948
VicenteVD <vavetrano@gmail.com>
2025-06-04 10:57:36 +0000

    arreglo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function generarExcusa() {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let random = (array) => array[Math.floor(Math.random() * array.length)];
+
+  return random(who) + " " + random(action) + " " + random(what) + " " + random(when) + ".";
+}
+
+window.onload = function () {
+  document.getElementById("excusa").innerHTML = generarExcusa();
 };
